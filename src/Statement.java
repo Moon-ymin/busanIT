2cb8e1c74a8c6e3863545d7e9e96e6aada67fcbc
Moon-ymin <ansdudals8@naver.com>
2024-02-06 13:50:16 +0900

    2월 6일 커밋, 자바 첫 실행, 주석 및 HelloWorld.

---
+++ src/Statement.java
@@ -0,0 +1,8 @@
+public class Statement {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 3;
+        int result = x + y; // 실행문의 범위는 ; 만날때까지
+        System.out.println(result);
+    }
+}
