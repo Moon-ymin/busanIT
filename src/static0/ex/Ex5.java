8497c2745b304f268af3623dffb759b9a374bdd2
Moon-ymin <ansdudals8@naver.com>
2024-02-22 11:10:43 +0900

    2월 22일, singleton, 상수, final키워드

---
+++ src/static0/ex/Ex5.java
@@ -0,0 +1,12 @@
+package static0.ex;
+
+public class Ex5 {
+    public static void main(String[] args) {
+        Account account = new Account("3333-08-6337279",
+                "홍길동",50000);
+
+        account.printAccount();
+
+    }
+
+}
