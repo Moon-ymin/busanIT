3d6dacce963ead954bda251e482167fec93331e7
Moon-ymin <ansdudals8@naver.com>
2024-03-08 09:48:21 +0900

    3월 7일, 연습문제

--- src/library/util/ex/Ex3.java
+++ src/library/util/ex/Ex3.java
@@ -9,15 +9,24 @@ public class Ex3 {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("원금: ");
-        BigDecimal Principal = new BigDecimal(sc.nextLine());
+        BigDecimal Principal = sc.nextBigDecimal();
+        // 소수점의 계산이 중요하기 때문에 BigDecimal로 받음
         System.out.print("연이율: ");
-        BigDecimal AnnualInterestRate = new BigDecimal(sc.nextLine());
+        BigDecimal AnnualInterestRate = sc.nextBigDecimal();
+        
         System.out.print("기간(년 단위): ");
         int TermInYear = sc.nextInt();
+        
+        BigDecimal num = BigDecimal.ONE.add(AnnualInterestRate); // AnnualInterestRate.add(new BigDecimal("1"))
+        BigDecimal total = Principal.multiply( num.pow(TermInYear) ); // pow안에는 int형 데이터 들어가야 함
 
-        BigDecimal num = BigDecimal.ONE.add(AnnualInterestRate);
-        BigDecimal total = Principal.multiply( num.pow(TermInYear) );
+        /* 이런식으로 표기도 가능
+        BigDecimal total = Principal
+                              .multiply(BigDecimal.ONE
+                              .add(AnnualInterestRate)
+                              .pow(TermInYear));
+         */
 
-        System.out.printf("최종 금액: %.2f", total);
+        System.out.printf("최종 금액: %,.2f원", total);
     }
 }
