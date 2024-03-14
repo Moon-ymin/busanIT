8387a00b719029b720ab81d51baceca53f0dfe55
Moon-ymin <ansdudals8@naver.com>
2024-03-14 10:42:06 +0900

    3월 14일 | Enum 활용 Comparator 연습문제

---
+++ src/collection/set/compare/StudentMain.java
@@ -0,0 +1,66 @@
+package collection.set.compare;
+
+
+import java.util.Arrays;
+import java.util.TreeSet;
+
+public class StudentMain {
+    public static void main(String[] args) {
+        Student 홍길동 = new Student("홍길동",30, 170.0);
+        Student 임꺽정 = new Student("임꺽정",40, 180.4);
+        Student 전우치 = new Student("전우치",25, 175.2);
+        Student 허생 = new Student("허생",45, 163.6);
+
+        TreeSet<Student> studentTreeSet = new TreeSet<>();
+
+        studentTreeSet.addAll(
+                Arrays.asList(홍길동, 임꺽정, 전우치, 허생)
+        );
+
+        for(Student s : studentTreeSet){
+            System.out.println(s);
+        }
+
+        System.out.println("===== 이름 순 정렬 =====");
+        // 이름 비교자 생성
+        StudentComparator namestudentComparator
+                = new StudentComparator(
+                        StudentComparator.SortBy.NAME,
+                        StudentComparator.SortDirection.ASC);
+        // 정렬 기준으로 이름 비교자
+        TreeSet<Student> nameSortSet = new TreeSet<>(namestudentComparator);
+        // 정렬 대상 삽입
+        nameSortSet.addAll(
+                Arrays.asList(홍길동, 임꺽정, 전우치, 허생)
+        );
+
+        for(Student s : nameSortSet) {
+            System.out.println(s);
+        }
+
+        TreeSet[] treeSets = {
+                new TreeSet<Student>(
+                        new StudentComparator(
+                                StudentComparator.SortBy.AGE,
+                                StudentComparator.SortDirection.ASC
+                        )
+                ),
+                new TreeSet<Student>(
+                        new StudentComparator(
+                                StudentComparator.SortBy.HEIGHT,
+                                StudentComparator.SortDirection.DESC
+                        )
+                )
+
+        };
+        for (TreeSet<Student> treeSet : treeSets) {
+            treeSet.addAll(
+                    Arrays.asList(홍길동, 임꺽정, 전우치, 허생)
+            );
+            System.out.println("============================");
+            for (Student student : treeSet) {
+                System.out.println(student);
+            }
+        }
+    }
+}
