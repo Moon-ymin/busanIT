5746fd77b475b0416c9af8f6435ec17bdf773425
moonyoungmin <ansdudals8@naver.com>
2024-03-28 00:08:32 +0900

    3월 27일 | File 메서드 연습문제

--- src/io_stream/file/ex/Ex1.java
+++ src/io_stream/file/ex/Ex1.java
@@ -1,53 +1,51 @@
 package io_stream.file.ex;
 
-import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.FileAlreadyExistsException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Stream;
 
 public class Ex1 {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        Path currentPath = Paths.get("").toAbsolutePath();
+        boolean isFinish = false;
+        while(!isFinish) {
+            System.out.println("1: 정보 저장, 2: 정보 조회");
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.print("이름: ");
+                    String name = sc.next();
+                    System.out.print("이메일: ");
+                    String email = sc.next();
+                    System.out.print("나이: ");
+                    String age = sc.next();
 
-        System.out.println("1: 정보 저장, 2: 정보 조회");
-        int choice = sc.nextInt();
-        if (choice == 1) {
-            System.out.print("이름: ");
-            String name = sc.next();
-            System.out.print("이메일: ");
-            String email = sc.next();
-            System.out.print("나이: ");
-            String age = sc.next();
-            List<String> stringList = Arrays.asList(
-                    name, email, age
-            );
-
-            Path newFilePath = currentPath.resolve(name+".user");
-            // 파일 생성
-            try {
-                Files.createFile(newFilePath);
-            } catch (FileAlreadyExistsException e) {
-                // 파일이 존재할 경우 덮어쓰기 하는 대신 예외 발생
-                System.out.println("파일이 이미 존재합니다.");
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+                    String filename = name + ".user";
+                    FileWriter writer = new FileWriter(filename);
+                    writer.write(name + "\n" + email + "\n" + age);
+                    writer.close();
+                    System.out.println("정보 저장 완료.");
+                    break;
+                case 2:
+                    File folder = new File(".");
+                    File[] files = folder.listFiles(
+                            (dir, fileName) -> fileName.endsWith(".user"));
+                    if (files != null && files.length > 0) {
+                        for (File f : files) {
+                            System.out.println(f.getName());
+                        }
+                    } else {
+                        System.out.println("저장된 파일이 없습니다.");
+                    }
+                    isFinish = true;
+                    break;
+                default:
+                    System.out.println("잘 못 입력하셨습니다.");
+                    break;
             }
-
-            Files.write(newFilePath, stringList);
-            System.out.println("정보 저장 완료.");
-        } else if (choice == 2){
-            //??
-        } else {
-            System.out.println("잘 못 입력하셨습니다.");
         }
-
     }
 }
--- src/io_stream/file/ex/Ex2.java
+++ src/io_stream/file/ex/Ex2.java
@@ -1,28 +1,39 @@
 package io_stream.file.ex;
 
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
+import java.io.*;
 import java.util.Scanner;
 
 public class Ex2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        List<String> text = new ArrayList<>();
-        FileReader fileReader = new FileReader();
 
         System.out.print("병합할 파일의 개수를 입력하세요: ");
-        int count = sc.nextInt();
-        for (int i = 0; i < count; i++) {
+        int cnt = sc.nextInt();
+
+        String[] filePaths = new String[cnt];
+        for (int i = 0; i < cnt; i++) {
             System.out.print("파일 경로를 입력하세요: ");
-            String filePath = sc.nextLine();
-            sc.nextLine();
-
-            fileReader(filePath);
-
-
+            filePaths[i] = sc.next();
         }
-        System.out.println("파일 병합 완료.");
+
+        String mergedPath = "src/io_stream/file/ex/merged.txt";
+        FileWriter fw = new FileWriter(mergedPath);
+        for (String filepath : filePaths) {
+            File file = new File(filepath);
+            if (!file.exists()) {
+                System.out.println(filepath + "에 파일이 존재하지 않습니다.");
+                return;
+            }
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            while ( (line = br.readLine()) != null ){
+                fw.write(line);
+                fw.write("\n");
+            }
+            fw.write("-----\n");
+        }
+        fw.close();
+        System.out.println("파일 병합 완료");
     }
 }
