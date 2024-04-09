25c42acf1c8b55b98e663df0f6165eaa39b73784
Moon-ymin <ansdudals8@naver.com>
2024-04-09 16:29:37 +0900

    4월 9일 | 연습문제

--- 데이터조회(SELECT)/서브쿼리.sql
+++ 데이터조회(SELECT)/서브쿼리.sql
@@ -28,25 +28,6 @@ select ename, sal from emp where sal > 1566.6667;
 select ename, sal from emp 
 where sal > (select avg(sal) from emp where deptno = 30);
 
--- ANY 함수
-SELECT sal FROM emp WHERE deptno = 20; -- 서브쿼리의 결과
- -- 20번 부서 직원의 급여 (5행) (800, 2975, 3000, 1100, 3000)
-
--- 메인 쿼리의 any 함수에 사용 (비교연산자와 함께 사용)
--- 하나라도 큰 것이 있으면 참 => 800보다 많이 받는 모든 직원
-SELECT ename, sal FROM emp 
-WHERE sal > ANY(SELECT sal FROM emp WHERE deptno = 20);
-
--- ALL 함수
--- (800, 2975, 3000, 1100, 3000) 여러 행의 결과 값 모두보다 이상이여야 참
--- 3000(최대값)보다 큰 경우만 참
-SELECT ename, sal FROM emp 
-WHERE sal >= ALL(SELECT sal FROM emp WHERE deptno = 20);
-
--- 800(최소값)보다 작은 경우만 참
-SELECT ename, sal FROM emp 
-WHERE sal <= ALL(SELECT sal FROM emp WHERE deptno = 20);
-
 -- IN 함수 : 목록 안에 값이 포함되었는지 확인
 SELECT * FROM emp WHERE deptno IN(10, 20, 30);
 
@@ -68,3 +49,52 @@ WHERE deptno IN(SELECT deptno from dept where dname = 'sales' or dname = 'resear
 -- 아래 실행문과 동일한 결과를 갖는다.
 SELECT * FROM emp 
 WHERE deptno IN(20, 30);
+
+
+
+-- ANY 함수
+SELECT sal FROM emp WHERE deptno = 20; -- 서브쿼리의 결과
+ -- 20번 부서 직원의 급여 (5행) (800, 2975, 3000, 1100, 3000)
+
+-- 메인 쿼리의 any 함수에 사용 (비교연산자와 함께 사용)
+-- 하나라도 큰 것이 있으면 참 => 800보다 많이 받는 모든 직원
+SELECT ename, sal FROM emp 
+WHERE sal > ANY(SELECT sal FROM emp WHERE deptno = 20);
+
+-- ALL 함수
+-- (800, 2975, 3000, 1100, 3000) 여러 행의 결과 값 모두보다 이상이여야 참
+-- 3000(최대값)보다 큰 경우만 참
+SELECT ename, sal FROM emp 
+WHERE sal >= ALL(SELECT sal FROM emp WHERE deptno = 20);
+
+-- 800(최소값)보다 작은 경우만 참
+SELECT ename, sal FROM emp 
+WHERE sal <= ALL(SELECT sal FROM emp WHERE deptno = 20);
+
+-- EXISTS : 주로 상호 연관 서브쿼리에서 유용하게 사용
+-- 상호연관 쿼리 : 메인 쿼리의 칼럼을 서브쿼리에서 사용하는 것
+-- 각 부서에 대해 직원이 존재하는 경우 참, 존재하지 않으면 거짓
+SELECT dname, loc FROM dept d
+WHERE EXISTS( SELECT 1 FROM emp e WHERE e.deptno = d.deptno );
+
+-- SELECT 절에서 서브쿼리 사용
+SELECT e.ename,
+	   e.sal,
+       e.deptno,
+       -- 각 부서의 평균 급여 => 메인쿼리의 컬럼의 하나로 사용
+	   (select avg(sal) from emp WHERE deptno = e.deptno) as "부서평균급여"
+FROM emp e;
+-- 단일 행을 반환하여 사용
+-- 데이터베이스에 따라 성능 및 비용 문제가 발생할 수 있음. 데이터 양이 많을 경우 다른 방법을 사용
+-- 쿼리의 결과를 유연하게 동적으로 표현하고 싶을 때 사용
+
+-- FROM절 : 인라인 뷰(Inline View)
+-- 서브쿼리가 임시 테이블처럼 동작하게 하여 메인 쿼리에 사용
+-- 생성된 임시 테이블은 쿼리 실행시점에만 존재하고 사라짐. => 쿼리문 안에서만 사용
+-- FROM 절의 임시 테이블은 약칭을 주어 사용해야 한다.
+
+-- 부서별 급여평균
+SELECT deptno, avg(sal) FROM emp GROUP BY deptno;
+
+SELECT dept_avg.deptno, dept_avg.avg_sal
+FROM ( SELECT deptno, avg(sal) as avg_sal FROM emp GROUP BY deptno ) as dept_avg;
