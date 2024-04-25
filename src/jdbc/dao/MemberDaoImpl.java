dded4ebe1839dbd3a68eff0a978955bd8821655e
moonyoungmin <ansdudals8@naver.com>
2024-04-25 13:11:01 +0900

    4월 25일 | DTO, DAO, VO

--- src/jdbc/dao/MemberDaoImpl.java
+++ src/jdbc/dao/MemberDaoImpl.java
@@ -4,14 +4,16 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 // DAO 인터페이스 구현 객체
 public class MemberDaoImpl implements MemberDao {
     Connection conn;
 
     // DAO 생성과 동시에 접근 객체 생성
-    public MemberDaoImpl() {
-        this.conn = DatabaseUtil.getConnection();
+    public MemberDaoImpl(Connection conn) {
+        this.conn = conn;
     }
 
     @Override
@@ -62,11 +64,62 @@ public class MemberDaoImpl implements MemberDao {
 
     @Override
     public void update(MemberDto member) {
+        String sql = """
+                UPDATE members SET name = ?, email = ?
+                WHERE id = ?
+                """;
+        PreparedStatement pstmt = null;
+        try {
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, member.getName());
+            pstmt.setString(2, member.getEmail());
+            pstmt.setString(3, member.getId());
+            pstmt.executeUpdate();
+            pstmt.close();
 
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void delete(String id) {
+        String sql = """
+                DELETE FROM members
+                WHERE id = ?
+                """;
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, id);
+            pstmt.executeUpdate();
+            pstmt.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    @Override
+    public List<MemberDto> getAllMembers() {
+        // 데이터를 전달할 객체 리스트 (빈 컬렉션) 생성
+        List<MemberDto> members = new ArrayList<>();
+        String sql = "SELECT * FROM MEMBERS";
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            // 여러개의 SQL문 결과 셋 반복 (커서 이동)
+            while (rs.next()) {
+                // DTO 객체를 생성하여 빈 컬렉션에 추가
+                MemberDto member = new MemberDto(
+                        rs.getString("id"),
+                        rs.getString("name"),
+                        rs.getString("email")
+                );
+                members.add(member);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        // 결과 셋을 담은 컬렉션을 반환
+        return members;
     }
 }
--- src/jdbc/dao/MemberMain.java
+++ src/jdbc/dao/MemberMain.java
@@ -1,39 +1,51 @@
 package jdbc.dao;
 
+import java.util.List;
 import java.util.Scanner;
 
 // 메인 실행계층에서는 데이터 접근과 관련된 모든 코드를 DAO에 위임
 public class MemberMain {
+    private static Scanner sc = new Scanner(System.in);
+    private static MemberDao memberDao;
     public static void main(String[] args) {
+
+        // DAO 연결 객체 초기화
+        memberDao = new MemberDaoImpl(DatabaseUtil.getConnection());
+
         // 데이터베이스 접근 객체 생성
-        MemberDao memberDao = new MemberDaoImpl();
+        while (true) {
+            System.out.print("1. 회원생성 | 2. 회원조회 | 3. 회원정보수정 | 4. 회원삭제  | 5. 전체회원목록 | 6. 나가기 > ");
+            String menu = sc.nextLine();
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("1. insert | 2. select > ");
-        String menu = scanner.nextLine();
-
-        switch (menu) {
-            case "1" -> insert(memberDao);
-            case "2" -> select()
+            switch (menu) {
+                case "1" -> insert();
+                case "2" -> select();
+                case "3" -> update();
+                case "4" -> delete();
+                case "5" -> list();
+                case "6" -> {DatabaseUtil.close(); return;}
+            }
         }
-
-
-        // SELECT
-//        MemberDto hong = memberDao.getMemberById("hong");
-//        System.out.println(hong.getId() + " " +  hong.getName()+ " " + hong.getEmail());
+    }
+    private static void list(){
+        List<MemberDto> allMembers = memberDao.getAllMembers();
+        System.out.printf("%-10s %-16s %-30s\n","아이디", "회원이름", "이메일");
+        System.out.println("----------------------------------------------");
+        for (MemberDto member : allMembers) {
+            System.out.printf("%-10s %-16s %-30s\n",
+                    member.getId(), member.getName(), member.getEmail());
+        }
     }
 
-    private static void insert(MemberDao memberDao) {
-        Scanner scanner = new Scanner(System.in);
-
+    private static void insert() {
         // 회원 가입
         System.out.println("[회원 생성]");
         System.out.print("id : ");
-        String id = scanner.nextLine();
+        String id = sc.nextLine();
         System.out.print("이름 : ");
-        String name = scanner.nextLine();
+        String name = sc.nextLine();
         System.out.print("이메일 : ");
-        String email = scanner.nextLine();
+        String email = sc.nextLine();
 
         // DTO 객체 생성
         MemberDto memberDto = new MemberDto(id, name, email);
@@ -42,7 +54,46 @@ public class MemberMain {
         memberDao.insert(memberDto);
     }
 
-    private static void select(String id) {
-        Scanner scanner = new Scanner(System.in);
+    private static void select() {
+        // SELECT
+        System.out.print("조회할 id를 입력하세요> ");
+        String id = sc.nextLine();
+        MemberDto hong = memberDao.getMemberById(id);
+        System.out.println("ID : " + hong.getId());
+        System.out.println("이름 : " + hong.getName());
+        System.out.println("이메일 : " + hong.getEmail());
+    }
+    private static void update(){
+        // Update 메서드 구현
+        // 조회하기 위해서는 ID(식별자)가 필요
+        System.out.print("수정할 회원 ID를 입력하세요 : ");
+        String id = sc.nextLine();
+        // 수정하기 위해서는 기존 정보가 필요 => SELECT 로 조회
+        MemberDto member = memberDao.getMemberById(id);
+
+        if (member != null) {
+            System.out.print("변경할 이름을 입력하세요 : ");
+            String name = sc.nextLine();
+            if (!name.isEmpty()) member.setName(name);
+            System.out.print("변경할 이메일을 입력하세요 : ");
+            String email = sc.nextLine();
+            if (!email.isEmpty()) member.setEmail(email);
+
+            memberDao.update(member);   // 데이터베이스 접근 위임
+            System.out.println("회원 정보가 변경되었습니다.");
+        } else {
+            System.out.println("존재하지 않는 회원입니다.");
+        }
+    }
+    private static void delete() {
+        // DELETE 메서드 구현
+        System.out.print("삭제할 회원 ID를 입력하세요 : ");
+        String id = sc.nextLine();
+        if (memberDao.getMemberById(id) != null) {
+            memberDao.delete(id);
+            System.out.println(id + " 회원이 삭제되었습니다.");
+        } else {
+            System.out.println("존재하지 않는 회원입니다.");
+        }
     }
 }
