a8c0fda6bce122b983f7ffe9c6939d9a7a083579
Moon-ymin <ansdudals8@naver.com>
2024-04-30 12:52:17 +0900

    4월 30일 | 코틀린 함수 선언과 호출

---
+++ src/h_exception/CheckedExceptionEx.java
@@ -0,0 +1,22 @@
+package h_exception;
+
+import java.io.*;
+
+// 자바는 메서드의 CheckedException을 처리해야 함.
+public class CheckedExceptionEx {
+    public void readFile() {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("README.md"));
+
+            String line = bufferedReader.readLine();
+            System.out.println(line);
+
+            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
