d7c66270a3d6cfa570b37a4614747aa1e56cb00c
Moon-ymin <ansdudals8@naver.com>
2024-05-14 15:52:12 +0900

    5월 14일 | android resource

---
+++ ch03_resource/src/main/java/com/busanit/ch03_resource/Resource1Activity.kt
@@ -0,0 +1,14 @@
+package com.busanit.ch03_resource
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Resource1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_resource1)
+    }
+}
