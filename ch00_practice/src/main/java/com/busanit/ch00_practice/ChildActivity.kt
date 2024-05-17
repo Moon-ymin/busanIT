4b1c261172a8a0ce9819a0da07818781e78b2f43
Moon-ymin <ansdudals8@naver.com>
2024-05-17 16:27:20 +0900

    5월 17일 | 연습문제

---
+++ ch00_practice/src/main/java/com/busanit/ch00_practice/ChildActivity.kt
@@ -0,0 +1,30 @@
+package com.busanit.ch00_practice
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.busanit.ch00_practice.databinding.ActivityChildBinding
+
+class ChildActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityChildBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 툴바 설정
+        setSupportActionBar(binding.toolbar)
+
+        // 업 버튼 활성화
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
