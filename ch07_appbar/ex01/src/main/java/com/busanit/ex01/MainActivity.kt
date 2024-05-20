cd7a9a69e97376b8988d2c5f0be3f8611d996110
Moon-ymin <ansdudals8@naver.com>
2024-05-20 16:17:44 +0900

    5월 20일 | 연습문제 다시보기

--- ch07_appbar/ex01/src/main/java/com/busanit/ex01/MainActivity.kt
+++ ch07_appbar/ex01/src/main/java/com/busanit/ex01/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.busanit.ex01
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -25,7 +26,11 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.search -> {
-                Toast.makeText(this, "search 선택", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "search 선택, Child 액티비티 실행합니다.", Toast.LENGTH_SHORT).show()
+                
+                // 메뉴 선택시 액티비티 전환
+                val intent = Intent(this, ChildActivity::class.java)
+                startActivity(intent)
             }
             R.id.setting -> {
                 Toast.makeText(this, "setting 선택", Toast.LENGTH_SHORT).show()
