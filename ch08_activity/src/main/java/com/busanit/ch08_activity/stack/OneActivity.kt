9b3be9dfd01d6764fc5678facc9ab44071d7382c
Moon-ymin <ansdudals8@naver.com>
2024-05-21 10:22:25 +0900

    5월 21일 | Back Stack & Task, Intent flags

--- ch08_activity/src/main/java/com/busanit/ch08_activity/stack/OneActivity.kt
+++ ch08_activity/src/main/java/com/busanit/ch08_activity/stack/OneActivity.kt
@@ -1,21 +1,32 @@
 package com.busanit.ch08_activity.stack
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.busanit.ch08_activity.R
+import com.busanit.ch08_activity.databinding.ActivityOneBinding
 
 class OneActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_one)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val binding = ActivityOneBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.run {
+            oneTextView.setOnClickListener {
+                startActivity(Intent(this@OneActivity,
+                    OneActivity::class.java))
+            }
+
+            twoTextView.setOnClickListener {
+                startActivity(Intent(this@OneActivity,
+                    TwoActivity::class.java))
+            }
+
+            threeTextView.setOnClickListener {
+                startActivity(Intent(this@OneActivity,
+                    ThreeActivity::class.java))
+            }
         }
     }
 }
