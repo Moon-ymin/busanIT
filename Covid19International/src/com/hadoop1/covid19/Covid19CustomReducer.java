093a5ab119eb4b7aa290b29171ce5e7a45b42c40
Youngmin Moon <83321379+Moon-ymin@users.noreply.github.com>
2024-06-05 16:13:29 +0900

    Update Covid19CustomReducer.java

--- Covid19International/src/com/hadoop1/covid19/Covid19CustomReducer.java
+++ Covid19International/src/com/hadoop1/covid19/Covid19CustomReducer.java
@@ -23,6 +23,9 @@ public class Covid19CustomReducer extends Reducer<Text, Covid19CustomWritable, T
 				System.out.println(System.identityHashCode(val));
 			}
 			if (max < val.getMax().get()) {
+/* 하둡에서는 데이터마다 객체를 생성하지 않고, 
+인터페이스를 만들고 그 안에 데이터를 넣는 형태로 진행하고 있기 때문에,(주소값이 전부 같아져버림)
+해시 코드 출력하면 전부 같은거로 나옴  System.identityHashCode(val) */
 				//maxWritable = val;
 				max = val.getMax().get();
 				maxWritable.setDate(new Text(val.getDate().toString()));
